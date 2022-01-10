c44362b3be2327ad25a5360704c74f251224b3ca
Ashik Rahman <ashrhmn99@gmail.com>
2022-01-11 01:19:35 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{tsx,jsx,ts,js}",
+        "./components/**/*.{tsx,jsx,ts,js}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
