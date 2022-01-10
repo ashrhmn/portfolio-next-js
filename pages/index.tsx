0bcbaa4248a8fb6f2019da783f78b206fc992bb7
Ashik Rahman <ashrhmn99@gmail.com>
2022-01-11 03:22:16 +0600

    add transition to all

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,14 @@ const Home: NextPage = () => {
         <title>Ashik Rahman</title>
       </Head>
       <main className="text-center">
-        <h1 className="text-5xl md:text-7xl">Hello World!!</h1>
-        <p className="text-3xl md:text-4xl mb-8">
+        <h1 className="text-5xl md:text-7xl transition-all">Hello World!!</h1>
+        <p className="text-3xl md:text-4xl mb-8 transition-all">
           Website is under development
         </p>
-        <span className="text-lg md:text-2xl">
+        <span className="text-lg md:text-2xl transition-all">
           Checkout older version on{" "}
           <a
-            className="text-blue-700 p-1 rounded-lg hover:scale-105 hover:text-blue-50 hover:bg-blue-600 transition-all duration-300"
+            className="text-blue-700 p-1 rounded-lg hover:scale-150 hover:text-blue-50 hover:bg-blue-600 transition-all duration-300"
             href="https://www.ashrhmn.com"
           >
             Ashik Rahman
