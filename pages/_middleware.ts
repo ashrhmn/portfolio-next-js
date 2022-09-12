02049516152759ee4b01a0fc19292e0775f575c0
ashrhmn <ashrhmn99@gmail.com>
2022-09-12 23:25:39 +0600

    stringify logger

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -8,6 +8,12 @@ export function middleware(request: NextRequest) {
     geo: JSON.stringify(geo, null, 2),
     headers: JSON.stringify(headers, null, 2),
   });
+  const obj: any = {};
+  headers.forEach((v, k) => {
+    obj[k] = v;
+  });
+  console.log({ obj });
+
   return NextResponse.next();
   //   return NextResponse.redirect(new URL("/about-2", request.url));
 }
