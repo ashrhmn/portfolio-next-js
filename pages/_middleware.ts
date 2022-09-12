361394ddf195c25ea2d7ac9b6cb1216803c0a9ee
ashrhmn <ashrhmn99@gmail.com>
2022-09-12 23:15:38 +0600

    stringify logger

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,7 +4,10 @@ import { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const geo = request.geo;
   const headers = request.headers;
-  console.log({ geo, headers });
+  console.log({
+    geo: JSON.stringify(geo, null, 2),
+    headers: JSON.stringify(headers, null, 2),
+  });
   return NextResponse.next();
   //   return NextResponse.redirect(new URL("/about-2", request.url));
 }
